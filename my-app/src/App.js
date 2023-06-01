b6309438eb76ff232823258f87261633c934fa33
Julia Scherbina <juliascherbina15@gmail.com>
2023-06-01 19:40:52 +0300

    add pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import '../src/shopping-car.css'
-
+import './styles.css'
+import Shop from './shop'
 function App() {
-  return (
-    <div className="App">
-      <h3>
-        <a href=""> Shop</a>
-      </h3>
-      <h3>
-        <a href=""> Shopping cart</a>
-      </h3>
-      <div className="content">
-        <div class="sidebar">
-          <h3>Shops:</h3>
-          <div className="title">
-            <span>McDonald's</span>
-          </div>
-          <div className="title">
-            <span>Namelaka</span>
-          </div>
-        </div>
-        <div className="food-choices">
-          <div className="units">
-            <img src="./imgs/BigTasty-Classic.jpg" alt=""/>
-              <p>Big Tasty</p>
-              <button>add to Cart</button>
-          </div>
-          <div className="units">
-            <img src="" alt=""/>
-              <p>Big Tasty</p>
-              <button>add to Cart</button>
-          </div>
-          <div className="units">
-            <img src="" alt=""/>
-              <p>Big Tasty</p>
-              <button>add to Cart</button>
-          </div>
-          <div className="units">
-            <img src="" alt=""/>
-              <p>Big Tasty</p>
-              <button>add to Cart</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Shop/>
 }
 
 export default App;
